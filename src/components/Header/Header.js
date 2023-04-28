f1e3dbe71f7ccd85e9a8183256f30d2360fa9ab4
ValeriiaMalyshko <valeria_goroshko@ukr.net>
2023-04-28 10:52:59 +0300

    add style to product list

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import s from './Header.module.css';
+import s from "./Header.module.css";
 import icon from "../../assets/pizza.svg";
 
 export default function Header() {
   return (
     <>
-      <header>
-        <div>
-          <img src={icon} alt="Pizza" width={44} height={35}></img>
-          <p>PizzaStyle</p>
+      <header className={s.header}>
+        <div className={s.card}>
+          <img
+            src={icon}
+            alt="Pizza"
+            width={44}
+            height={35}
+            className={s.img}
+          ></img>
+          <p className={s.name}>PizzaStyle</p>
         </div>
-        <nav>
-          <Link to="/">Pizza</Link>
-          <Link to="/cart">Cart</Link>
+        <nav className={s.nav}>
+          <Link to="/" className={s.link}>
+            Pizza
+          </Link>
+          <Link to="/cart" className={s.link}>
+            Cart
+          </Link>
         </nav>
       </header>
     </>
--- src/pages/CartPage/Cart.js
+++ src/pages/CartPage/Cart.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // import s from "./UserList.module.css";
 
 const Cart = () => {
-  return <></>;
+  return (
+    <>
+      <h2>
+        Please, choose pizza <Link to="/">here</Link>
+      </h2>
+    </>
+  );
 };
 
 export default Cart;
